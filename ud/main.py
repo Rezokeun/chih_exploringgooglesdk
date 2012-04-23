bb61a986b4cbfed56b44979d9d4cefcb20be49b1
Chih Chow <chih.chow@analoganalytics.com>
2012-04-23 16:58:03 -0700

    Edits to main.py

--- ud/main.py
+++ ud/main.py
@@ -16,9 +16,22 @@
 #
 import webapp2
 
+form="""
+<form action="/testform">
+    <input name="q">
+    <input type="submit">
+</form>
+"""
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+       #self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write(form)
+
+class TestHandler(webapp2.RequestHandler):
+    def get(self):
+        q = self.request.get("q")
+        self.response.out.write(q)
 
 app = webapp2.WSGIApplication([('/', MainHandler)],
                               debug=True)
